b63ed6c3294a9aa12cdbda47ad5941b7a7db368a
giacomocerre <cerre.giacomo93@gmail.com>
2024-04-19 19:28:20 +0200

    Add envs

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB47kghLSRqvmlqtOdtlmIdNSlGg7Jesng",
-  authDomain: "legapauperlivorno-a8228.firebaseapp.com",
-  projectId: "legapauperlivorno-a8228",
-  storageBucket: "legapauperlivorno-a8228.appspot.com",
-  messagingSenderId: "709133106077",
-  appId: "1:709133106077:web:a8cd4875fc312b961fcd74"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 // Initialize Firebase
