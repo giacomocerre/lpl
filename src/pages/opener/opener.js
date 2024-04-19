38978945761f47f1afe4dc63d585aed1d4113bee
giacomocerre <cerre.giacomo93@gmail.com>
2024-04-19 19:13:26 +0200

    Fix opener

--- src/pages/opener/opener.js
+++ src/pages/opener/opener.js
@@ -10,7 +10,7 @@ const Opener = () => {
         <div className="relative z-20 max-w-screen-lg mx-auto grid grid-cols-0 md:grid-cols-5 p-10 h-full items-center">
           <div className="col-span-6">
             <span className="uppercase text-md font-bold mb-2 block bg-white w-fit py-1 px-5 rounded-full text-black">WE ARE ALL PIRATES</span>
-            <h1 className="text-white font-extrabold text-6xl mb-8">Sito ufficiale delle <br/>Lega Pauper Livorno</h1>
+            <h1 className="text-white font-extrabold md:text-6xl text-5xl mb-8">Sito ufficiale <br/>Lega Pauper Livorno</h1>
             <p className="text-stone-100 text-md text-base">
               Esplorando i meandri della Lega: Un'esauriente rassegna degli eventi e delle curiosità che coinvolgono la nostra ciurma
             </p>
